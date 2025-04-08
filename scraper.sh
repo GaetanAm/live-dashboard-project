00c9eb5e1e7aac96a5cd279c948c9789731ea101
Gaëtan <gaetan.amiens@edu.devinci.fr>
2025-04-08 14:01:30 +0200

    Update scraper.sh

--- scraper.sh
+++ scraper.sh
@@ -3,10 +3,6 @@
 timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 html=$(curl -s -A "Mozilla/5.0" "https://www.investing.com/indices/us-30")
 
-value=$(echo "$html" | grep -oP '"instrument-price-last"[^>]*>\K[^<]+' | head -1 | tr -d ',')
+value=$(echo "$html" | grep -oP 'data-test="instrument-price-last">[\d,]+' | grep -oP '[\d,]+' | head -1 | tr -d ',')
 
 echo "$timestamp,$value" >> data.csv
-
-
-
-
